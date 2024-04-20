8dfc7145a103ebe3ecf0cb07c045e91da131b62c
Mohamed Ganoub <86044052+MGanoub@users.noreply.github.com>
2024-04-20 21:12:35 +0200

    Delete dating_app directory

--- dating_app/lib/screens/onboarding/widgets/widgets.dart
+++
@@ -1,6 +0,0 @@
-export "custom_button.dart";
-export "custom_textheader.dart";
-export "custom_textfield.dart";
-export "custom_checkbox.dart";
-export "custom_imagecontainer.dart";
-export "custom_textcontainer.dart";
--- dating_app/lib/screens/screens.dart
+++
@@ -1,4 +0,0 @@
-export 'package:dating_app/screens/users/user_screen.dart';
-export 'package:dating_app/screens/home/home_screen.dart';
-export 'package:dating_app/screens/onboarding/onboarding_screen.dart';
-export 'package:dating_app/screens/onboarding/onboarding_screens/screens.dart';
